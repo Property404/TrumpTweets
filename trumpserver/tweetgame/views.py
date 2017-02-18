47bf295a69c249dd60d232e6fddd20dbf5e6838a
root <root@Coffee.localdomain>
2017-02-18 07:48:34 +0000

    Added index with bootstrap headers

--- trumpserver/tweetgame/views.py
+++ trumpserver/tweetgame/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+main = open("/var/www/trumptweets/trumpserver/tweetgame/static/index.html").read();
 def index(request):
-    return HttpResponse("This is the future. This is the past. All are welcome, but None shall pass.");
+    return HttpResponse(main);
