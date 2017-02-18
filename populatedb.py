2bf0cd5dece5baee2465f3bb1e7fcc32fa9ef788
Property404 <daganboy@gmail.com>
2017-02-18 11:48:45 +0000

    database loader starting to work

--- populatedb.py
+++ populatedb.py
@@ -1,9 +1,14 @@
+import base64
 import tweepy
 import time
 import json
-import django
-from models.py import Tweet
-
+from secrets import*
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+import MySQLdb
+db = MySQLdb.connect("localhost","root","hunter2","trump");        
+c = db.cursor()
 def limit_handled(cursor):
     while True:
         try:
@@ -12,8 +17,25 @@ def limit_handled(cursor):
             time.sleep(15 * 60)
 
 
-for status in limit_handled(tweepy.Cursor(api.user_timeline,user_id = "25073877").items()):
-    parsed_json = json.loads(status)
+columns = ["id", "created_at","text","favorite_count","retweet_count","in_reply_to_user_id","in_reply_to_status_id"]
+def load_tweets(user_id):
+    tweets = []
+    for status in limit_handled(tweepy.Cursor(api.user_timeline,user_id = "25073877").items()):
+        parsed_json = status._json
+        values  = [str(parsed_json[x]) if x=="id" else (base64.b64encode(str(parsed_json[x]).encode("UTF8"))).decode("UTF8") for x in columns]
+        tweets += values
+        query = "INSERT INTO tweets ("+",".join(columns)+") VALUES("+values[0]+",\""+"\",\"".join(values[1::])+"\");";
+        print(query);
+        c.execute(query);
+        db.commit();
+        print(c.fetchall())
+        print(values);
+        time.sleep(10);
+
+
+load_tweets("25073877");
+"""
+
     twit = Tweet.objects.create(tweet_id = parsed_json['id'], created_at = parsed_json['created_at'], text = parsed_json['text'],
                           favorite_count = parsed_json['favorite_count'], retweet_count = parsed_json['retweet_count'],
                           in_reply_to_status_id= parsed_json['in_reply_to_status_id'], in_reply_to_user_id = parsed_json['in_reply_to_user_id'])
@@ -25,3 +47,4 @@ for status in limit_handled(tweepy.Cursor(api.user_timeline,user_id = "25073877"
                           favorite_count = parsed_json['favorite_count'], retweet_count = parsed_json['retweet_count'],
                           in_reply_to_status_id= parsed_json['in_reply_to_status_id'], in_reply_to_user_id = parsed_json['in_reply_to_user_id'])
    twit.save()
+   """
