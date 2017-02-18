7e692a45f313c8dc63532608e5b7b501a09cefe6
yungdarkgod <a.dmitrijoseph@gmaail.com>
2017-02-18 04:14:12 -0500

    null fields & Textfield

--- trumpserver/tweetgame/models.py
+++ trumpserver/tweetgame/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Tweet(models.Model):
     tweet_id = models.IntergerField(max_length = 25)
     created_at= models.CharField(max_length = 50)
-    text = models.Charfield(max_length = 200)
+    text = models.TextField
     favorite_count =  models.IntergerField(max_length = 25)
     retweet_count =  models.IntergerField(max_length = 25)
     in_reply_to_status_id = models.IntergerField(max_length = 25, null =True)
