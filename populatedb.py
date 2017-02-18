035be27b48de0d8360ef4f4b091f570e5ae0cbdd
yungdarkgod <a.dmitrijoseph@gmaail.com>
2017-02-18 14:53:45 -0500

    added more tracking oprtions

--- populatedb.py
+++ populatedb.py
@@ -9,6 +9,7 @@ api = tweepy.API(auth)
 import MySQLdb
 db = MySQLdb.connect("localhost","root","hunter2","trump");
 c = db.cursor()
+count = 0
 def limit_handled(cursor):
     while True:
         try:
@@ -20,19 +21,19 @@ while True:
     q= "SELECT id from tweets";
     c.execute(q);
     l = (c.fetchall());
-    maxid = l[0][0]
+    maxid = l[-1][0]
     columns = ["id", "created_at","text","favorite_count","retweet_count","in_reply_to_user_id","in_reply_to_status_id"]
     def load_tweets(user_id,max_id):
         tweets = []
         markov = ""
         print("after markov")
-        for status in limit_handled(tweepy.Cursor(api.user_timeline,user_id = "25073877",max_id = maxid).items()):
+        for status in limit_handled(tweepy.Cursor(api.user_timeline,user_id = "25073877",max_id= 0).items()):
             print("in tweet loop")
             parsed_json = status._json
             markov += parsed_json["text"];
             values  = [str(parsed_json[x]) if x=="id" else (base64.b64encode(str(parsed_json[x]).encode("UTF8"))).decode("UTF8") for x in columns]
             tweets += values
-            query = "INSERT IGNORE INTO tweets ("+",".join(columns)+") VALUES("+values[0]+",\""+"\",\"".join(values[1::])+"\");";
+            query = "INSERT IGNORE INTO tweets ("+",".join(columns)+") VALUES("+values[0]+",\""+"\",\"".join(values[1::])+"\");"
             print(query);
             c.execute(query);
             db.commit();
@@ -43,10 +44,9 @@ while True:
         a.close();
     print("before load")
     load_tweets(user_id= "25073877", max_id = maxid)
-    count = 0
     count +=1
-    if count == 900:
-        print('hit limit going to sleep be back in 15')
+    if count == 899:
+        print('\nhit limit going to sleep be back in 15\n')
         print(time.gmtime())
         time.sleep(15 * 60)
     continue
