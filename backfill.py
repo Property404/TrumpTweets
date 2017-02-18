f1d7ebdbf68c4805884e8df72ea03005c1cf89fd
yungdarkgod <a.dmitrijoseph@gmail.com>
2017-02-18 23:34:33 +0000

    we need to reseed

--- backfill.py
+++ backfill.py
@@ -1,15 +1,14 @@
-import base64
 import tweepy
+import base64
 import time
 import json
-from secrets import*
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-import time
-import json
+import io
 from secrets import*
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
+sleepy = api.rate_limit_status()
+print(sleepy)
 import MySQLdb
 db = MySQLdb.connect("localhost","root","hunter2","trump");
 c = db.cursor()
@@ -31,15 +30,15 @@ with open(r'/home/hackfsu/TrumpTweets/all_trump_tweets.json') as json_data:
     masterlist = json.load(json_data)
     masterlist.sort()
     for x in masterlist:
-        if int(x) > 142737148091707392:
+        if int(x) < 142737148091707392:
             masterlist.remove(x)
 
 for x in masterlist:
     status_json = api.get_status(x)
-    parsed_json = {'id' : status_json.id, 'created_at' : status_json.created_at, 'text':status_json.text, "favorite_count":  status_json.favorite_count,"retweet_count" : status_json.retweet_count,
+    parsed_json = {'id' : status_json.id, 'created_at' : status_json.created_at,'user_id': status_json.user_id, 'text':status_json.text, "favorite_count":  status_json.favorite_count,"retweet_count" : status_json.retweet_count,
                    "in_reply_to_user_id" : status_json.in_reply_to_user_id,
                    'in_reply_to_status_id' : status_json.in_reply_to_user_id}
-    columns = ["id", "created_at", "text", "favorite_count", "retweet_count", "in_reply_to_user_id",
+    columns = ["id", "created_at",'user_id', "text", "favorite_count", "retweet_count", "in_reply_to_user_id",
                "in_reply_to_status_id"]
     tweets = []
     markov = ""
@@ -62,8 +61,9 @@ for x in masterlist:
     if count == 850:
         with io.open(r'/home/hackfsu/TrumpTweets/all_trump_tweets.json', 'w') as json_data:
             json.dump(masterlist, json_data)
-            io.close(json_data)
+            os.close(json_data)
         print('\nhit limit going to sleep be back in 15\n')
         lastread = status_json.id
         time.sleep(15 * 60)
+        print(lastread)
     continue
