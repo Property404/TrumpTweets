4c48eefcdd54362300c0ed14f1909a0feb89c195
root <root@Coffee.localdomain>
2017-02-18 06:59:55 +0000

    Was able to access from otuside world

--- trumpserver/tweetgame/views.py
+++ trumpserver/tweetgame/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("This is the future. This is the past. All are welcome, but None shall pass.");
