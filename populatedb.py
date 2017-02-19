ed787d655c7e828f0171a08f1196312064ca6f05
yungdarkgod <a.dmitrijoseph@gmaail.com>
2017-02-19 03:17:15 -0500

    website format

--- populatedb.py
+++ populatedb.py
@@ -4,13 +4,12 @@ import time
 import json
 from secrets import*
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 import MySQLdb
 db = MySQLdb.connect("162.243.109.160","root","hunter2","trump");
 c = db.cursor()
 count = 0
-last_tweet
 def limit_handled(cursor):
     while True:
         try:
@@ -23,17 +22,17 @@ while True:
     c.execute(q);
     l = (c.fetchall());
     sinceid = l[-1][0]
-    print(sinceid['created_at'])
+    print(sinceid)
     columns = ["id", "created_at","text","favorite_count","retweet_count","in_reply_to_user_id","in_reply_to_status_id"]
-    def load_tweets(user_id,max_id):
+    def load_tweets(user_id,since_id):
         tweets = []
         markov = ""
         print("after markov")
-        for status in limit_handled(tweepy.Cursor(api.user_timeline,user_id = "25073877",since_id= sinceid).items()):
+        for Status in limit_handled(tweepy.Cursor(api.user_timeline,user_id = "25073877",since_id= sinceid).items()):
             print("in tweet loop")
-            parsed_json = status._json
-            markov += parsed_json["text"];
-            values  = [str(parsed_json[x]) if x=="id" else (base64.b64encode(str(parsed_json[x]).encode("UTF8"))).decode("UTF8") for x in columns]
+            parsed_json = Status_json
+            markov += parsed_json.text;
+            values  = [str(parsed_json.x) if x=="id" else (base64.b64encode(str(parsed_json[x]).encode("UTF8"))).decode("UTF8") for x in columns]
             tweets += values
             query = "INSERT IGNORE INTO tweets ("+",".join(columns)+") VALUES("+values[0]+",\""+"\",\"".join(values[1::])+"\");"
             print(query);
@@ -51,8 +50,8 @@ while True:
         print('\nhit limit going to sleep be back in 15\n')
         print(time.gmtime())
         time.sleep(15 * 60)
-    last_tweet = status_json['id']
-    print[last_tweet]
+    #last_tweet = status_json.id
+    #print[last_tweet]
 """
 
     twit = Tweet.objects.create(tweet_id = parsed_json['id'], created_at = parsed_json['created_at'], text = parsed_json['text'],
---
+++ secrets.py
@@ -0,0 +1,6 @@
+import tweepy
+
+consumer_key = 'QP7MbFO95FieQ5H7rAN0y4Pyx'
+consumer_secret = 'omILtAWJlKcrSrxcuxFSHe46SMzU00ufyebqJVzVO7CbUFtE8C'
+access_token = '832832529757564928-zFGSnlBgFZIzLR8lHDkzt1r2Mu3dGED'
+access_token_secret = 'shGM3d38uVk37zgMEbEu2e7nKF456sDxq9ml6yrfGbW1d'
